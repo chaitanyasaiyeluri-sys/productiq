713780ec9dc5f9a59374ce6ac72a914b4285d574
Y C NAGA DURGA SAI <275914886+chaitanyasaiyeluri-sys@users.noreply.github.com>
2026-08-22 18:54:44 +0000

    Production deployment preparation for Product Intelligence Hub

---
+++ isolate/assets/skeleton-FtQxmb9x.js
@@ -0,0 +1,1 @@
+import{j as o}from"./framer-motion-Cw2g8bUC.js";import{e as s}from"./index-DH6ol-qO.js";function r({className:e,...t}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",e),...t})}export{r as S};
